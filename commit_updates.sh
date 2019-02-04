5c3425cfbed5e8cd5bbceb17e88e5c9ff7fd55f4
Craig Post <cpost@bitbuckets.org>
2019-02-04 16:48:12 -0700

    Added script to commit updates

---
+++ commit_updates.sh
@@ -0,0 +1,20 @@
+#https://gist.github.com/willprice/e07efd73fb7f13f917ea
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git add .
+  git commit --message "Travis build of branch '$TRAVIS_REPO_SLUG' build number $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin-pages https://${GH_TOKEN}@github.com/cpostbitbuckets/maven_repo.git > /dev/null 2>&1
+  git push https://${GH_TOKEN}@github.com/cpostbitbuckets/maven_repo.git
+}
+
+setup_git
+commit_website_files
+upload_files
